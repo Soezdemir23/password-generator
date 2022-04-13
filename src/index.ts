f2aaa9e0b51fd058cbee8b11c5d58212961d24da
Soezdemir23 <Soezdemir3423@gmail.com>
2022-04-13 04:47:41 -0600

    initial commit: Create basic starter structure

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* index.ts
+Passowrd generator
+Requirements:
+Array to hold all possible chars
+Button to generate 4 random password options
+Display password ooptions
+Stretch: ability to set password length
+Stretch: 1-Click copy passowrd to the clipboard */
+
+console.log("hello, my name is maiksdf")
